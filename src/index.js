3f7675436dd52d693348ec9c9333e0852e7fdad3
Renan Veronez Drechsler <renanvz@outlook.com>
2020-11-08 11:01:55 -0300

    FlatList

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { View, ScrollView, Text, StyleSheet, StatusBar } from 'react-native';
+import { FlatList, Text, StyleSheet, StatusBar } from 'react-native';
 import api from './services/api';
 
 export default function App() {
@@ -18,11 +18,14 @@ export default function App() {
         <>
         <StatusBar barStyle="light-content" backgroundColor="#7159c1"/>
         
-        <ScrollView style={styles.container}>
-            {projects.map(project => (
-                <Text style={styles.project} key={project.id}>{project.title}</Text>
-            ))}
-        </ScrollView>
+        <FlatList
+            style={styles.container}
+            data={projects}
+            keyExtractor={project => project.id}
+            renderItem={({ item: project }) => (             //renderItem faz um return
+                <Text style={styles.project}>{project.title}</Text>
+            )}
+        />
         </>
     );
 }
@@ -35,6 +38,6 @@ const styles = StyleSheet.create({
 
     project: {
         color: '#FFF',
-        fontSize: 120,
+        fontSize: 30,
     },
 });
